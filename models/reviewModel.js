94705255c27ed426d535ab6013d37a25da3fddbf
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2023-02-07 13:57:54 +0100

    added review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,39 +1,45 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'Review can not be empty'],
-  },
-
-  rating: {
-    type: Number,
-    default: 0,
-    min: [1, "Review can't be lower than one"],
-    max: [5, "Review can't be higher than 5"],
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-
-  user: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: [true, 'Review must belong to a user.'],
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
     },
-  ],
 
-  tour: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Tour',
-      required: [true, 'Review must belong to a tour.'],
+    rating: {
+      type: Number,
+      default: 0,
+      min: [1, "Review can't be lower than one"],
+      max: [5, "Review can't be higher than 5"],
     },
-  ],
-});
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must belong to a user.'],
+      },
+    ],
+
+    tour: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Review must belong to a tour.'],
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Review = mongoose.model('Review', reviewSchema);
 
